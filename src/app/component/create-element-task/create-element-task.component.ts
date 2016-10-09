0863e6cd96a620a26147164c175e195e7c73d9cc
unknown <>
2016-10-09 17:57:23 +0800

    HTML

--- src/app/component/create-element-task/create-element-task.component.ts
+++ src/app/component/create-element-task/create-element-task.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'create-element-task',
   templateUrl: './create-element-task.component.html',
-  styleUrls: ['./create-element-task.component.css']
+  styleUrls: ['./../element-detail/element-detail.component.css','./create-element-task.component.css']
 })
 export class CreateElementTaskComponent implements OnInit {
 

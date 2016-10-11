cc0e599f01c743c0158a08fdfe75188e69e23a75
unknown <>
2016-10-11 17:11:46 +0800

    import modal类

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
-import { AlertModule, DatepickerModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { AlertModule, DatepickerModule,ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { routing } from './app.routing';
 import { AppComponent } from './component/app.component';

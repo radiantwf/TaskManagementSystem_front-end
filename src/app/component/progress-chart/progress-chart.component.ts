42a2f540c74bebd15b85565388ff9c66bb66c26d
radiantwf <radiantwf@gmail.com>
2016-10-18 02:01:58 +0800

    时间计算

--- src/app/component/progress-chart/progress-chart.component.ts
+++ src/app/component/progress-chart/progress-chart.component.ts
@@ -4,13 +4,40 @@ import { Component, OnInit } from '@angular/core';
   selector: 'progress-chart',
   templateUrl: './progress-chart.component.html',
   styleUrls: ['./progress-chart.component.css',
-  './../element-record/element-record.component.css']
+    './../element-record/element-record.component.css'],
+  inputs: ['planningBeginDate', 'planningEndDate', 'realBeginDate', 'realEndDate']
 })
 export class ProgressChartComponent implements OnInit {
-
+  planningBeginDate: Date;
+  planningEndDate: Date;
+  realBeginDate: Date;
+  realEndDate: Date;
+  private endDateDifference = 0;
+  private isNoPlanning = false;
+  private dateNow: Date;
   constructor() { }
 
   ngOnInit() {
+    this.dateNow = new Date(Date.now());
+    this.dateNow.setHours(0, 0, 0, 0);
+    if (this.planningBeginDate === null || this.planningEndDate === null) {
+      this.isNoPlanning = true;
+      return;
+    }
+    if (this.realBeginDate === null) {
+      this.endDateDifference = -1;
+      return;
+    }
+    if (this.realEndDate === null) {
+      var endTime1 = (new Date(this.planningEndDate)).getTime();
+      var endTime2 = this.dateNow.getTime();
+      this.endDateDifference = (endTime1 - endTime2) / 1000 / 3600 / 24;
+    }
+    else {
+      var endTime1 = (new Date(this.planningEndDate)).getTime();
+      var endTime2 = (new Date(this.realEndDate)).getTime();
+      this.endDateDifference = (endTime1 - endTime2) / 1000 / 3600 / 24;
+    }
   }
 
 }

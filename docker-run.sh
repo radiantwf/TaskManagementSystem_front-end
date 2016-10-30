61bbdb2bff4e048d2447170f43736f73150fb65d
radiantwf <radiantwf@gmail.com>
2016-10-30 21:07:11 +0800

    版本号修改

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 docker kill front;docker rm front
-docker rmi front-image 211.157.146.6:5000/task-management-frontend:0.01
+docker rmi front-image 211.157.146.6:5000/task-management-frontend
 rm -rf dist
 ng build --prod
 docker build -t front-image .
 
-docker tag front-image 211.157.146.6:5000/task-management-frontend:0.01
-docker push 211.157.146.6:5000/task-management-frontend:0.01
+docker tag front-image 211.157.146.6:5000/task-management-frontend
+docker push 211.157.146.6:5000/task-management-frontend
 docker rmi front-image
 
-docker run -d -p 4000:80 --name=front 211.157.146.6:5000/task-management-frontend:0.01
+docker run -d -p 4000:80 --name=front 211.157.146.6:5000/task-management-frontend

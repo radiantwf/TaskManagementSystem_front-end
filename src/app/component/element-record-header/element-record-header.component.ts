5a8c734364442af431192dc32733251cc71e8367
radiantwf <radiantwf@gmail.com>
2016-11-16 17:47:06 +0800

    权限修改

--- src/app/component/element-record-header/element-record-header.component.ts
+++ src/app/component/element-record-header/element-record-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Task } from './../../model/task';
 import { Router } from '@angular/router';
+import { AppGlobal } from '../../shared/app-global';
 
 @Component({
   selector: 'element-record-header',
@@ -11,13 +12,21 @@ import { Router } from '@angular/router';
 })
 export class ElementRecordHeaderComponent implements OnInit {
   private detailFlag = false;
-  taskRecord: Task;
+  private taskRecord: Task;
+  private detailClicked = new EventEmitter();
   private processFlag: boolean = false;
-  detailClicked = new EventEmitter();
   constructor(private router: Router) { }
 
   ngOnInit() {
+    var permissions = AppGlobal.getInstance().currentUser.permissions;
+    if (this.taskRecord.status == "新建" && permissions.findIndex(value => (value == 1
+      || value == 11 || value == 21 || value == 98)) >= 0) {
+      this.processFlag = true;
+    } else {
+      this.processFlag = false;
+    }
   }
+
   onDetailClicked(event) {
     this.detailClicked.emit(event);
   }

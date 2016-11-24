8cfa9746f05436d527be7ad004e4647d59d12533
radiantwf <radiantwf@gmail.com>
2016-11-24 16:21:26 +0800

    升级

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 npm i --save @angular/common @angular/compiler @angular/core @angular/forms @angular/http @angular/material @angular/platform-browser    @angular/platform-browser-dynamic    @angular/router @angular/upgrade angular-in-memory-web-api angular2-highcharts rxjs core-js 
-npm i --save-dev typescript
+npm i --save-dev typescript tslint

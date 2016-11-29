88c57ab51642aa778b5c6a3246056110400f2818
王峰 <wangfeng@wangfengdeMac-mini.local>
2016-11-29 19:50:04 +0800

    产品 项目修改

--- src/app/component/element-product-detail/element-product-detail.component.ts
+++ src/app/component/element-product-detail/element-product-detail.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from './../../model/product';
+import { ProductService } from './../../service/product.service';
+import { Router } from '@angular/router';
+import { AppGlobal } from '../../shared/app-global';
+import { MdDialogRef, MdDialog } from '@angular/material';
+import { DialogPlanComponent } from './../dialog-plan/dialog-plan.component';
 
 @Component({
   selector: 'app-element-product-detail',
   templateUrl: './element-product-detail.component.html',
-  styleUrls: ['./element-product-detail.component.css']
+  styleUrls: ['./../element-product-header/element-product-header.component.css', './element-product-detail.component.css']
 })
 export class ElementProductDetailComponent implements OnInit {
+  @Input() productId: string;
+  productRecord: Product = new Product('', '');
 
-  constructor() { }
+  isSeller: boolean = false;
+  isOC: boolean = false;
+  isProductAdmin: boolean = false;
+  isProductManager: boolean = false;
+  isAdmin: boolean = false;
+
+
+  accessAlert: boolean = false;
+  refuseAlert: boolean = false;
+
+  constructor(private productService: ProductService, public dialog: MdDialog, private router: Router) { }
 
   ngOnInit() {
-  }
+    let user = AppGlobal.getInstance().currentUser;
 
+    this.isAdmin = user.permissions.findIndex(value => (value === 1)) >= 0;
+    this.isOC = user.permissions.findIndex(value => (value === 99)) >= 0;
+    this.isSeller = user.permissions.findIndex(value => (value === 98)) >= 0;
+    this.isProductAdmin = user.permissions.findIndex(value => (value === 11 || value === 21)) >= 0;
+    this.isProductManager = user.permissions.findIndex(value => (value === 19 || value === 29)) >= 0;
+  }
 }
--- src/app/component/element-project-detail/element-project-detail.component.ts
+++ src/app/component/element-project-detail/element-project-detail.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from './../../model/project';
+import { ProjectService } from './../../service/project.service';
+import { Router } from '@angular/router';
+import { AppGlobal } from '../../shared/app-global';
+import { MdDialogRef, MdDialog } from '@angular/material';
+import { DialogPlanComponent } from './../dialog-plan/dialog-plan.component';
 
 @Component({
   selector: 'app-element-project-detail',
   templateUrl: './element-project-detail.component.html',
-  styleUrls: ['./element-project-detail.component.css']
+  styleUrls: ['./../element-project-header/element-project-header.component.css', './element-project-detail.component.css']
 })
 export class ElementProjectDetailComponent implements OnInit {
+  @Input() projectId: string;
+  projectRecord: Project = new Project('', '');
 
-  constructor() { }
+  isSeller: boolean = false;
+  isOC: boolean = false;
+  isProjectAdmin: boolean = false;
+  isProjectManager: boolean = false;
+  isAdmin: boolean = false;
+
+
+  accessAlert: boolean = false;
+  refuseAlert: boolean = false;
+
+  constructor(private projectService: ProjectService, public dialog: MdDialog, private router: Router) { }
 
   ngOnInit() {
-  }
+    let user = AppGlobal.getInstance().currentUser;
 
+    this.isAdmin = user.permissions.findIndex(value => (value === 1)) >= 0;
+    this.isOC = user.permissions.findIndex(value => (value === 99)) >= 0;
+    this.isSeller = user.permissions.findIndex(value => (value === 98)) >= 0;
+    this.isProjectAdmin = user.permissions.findIndex(value => (value === 11 || value === 21)) >= 0;
+    this.isProjectManager = user.permissions.findIndex(value => (value === 19 || value === 29)) >= 0;
+  }
 }

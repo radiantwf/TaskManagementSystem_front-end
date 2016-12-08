e17dce7f7460dd7a52d44069fb7bc7d216b37437
radiantwf <radiantwf@gmail.com>
2016-12-08 15:11:03 +0800

    css优化

--- src/app/component/communications/communications.component.ts
+++ src/app/component/communications/communications.component.ts
@@ -7,8 +7,7 @@ import { CommunicationsService } from './../../service/communications.service';
 @Component({
   selector: 'communications',
   templateUrl: './communications.component.html',
-  styleUrls: ['./communications.component.css',
-    './../element-task-header/element-task-header.component.css']
+  styleUrls: ['./communications.component.css']
 })
 export class CommunicationsComponent implements OnInit {
   list: Communication[] = [];

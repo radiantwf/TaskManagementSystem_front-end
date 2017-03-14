797b50ed5ab03ea5d1384e94cccfb318d6f831de
radiantwf <radiantwf@gmail.com>
2017-03-14 22:52:12 +0800

    升级，格式修改

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
-npm i --save @angular/common @angular/compiler @angular/core @angular/forms @angular/http @angular/platform-browser    @angular/platform-browser-dynamic    @angular/router @angular/upgrade angular-in-memory-web-api  @angular/material angular2-highcharts rxjs core-js 
-npm i --save-dev tslint @angular/compiler-cli
+rm -rf node_modules dist tmp
+npm i --registry https://registry.npm.taobao.org --save @angular/common @angular/compiler @angular/core @angular/forms @angular/http @angular/platform-browser    @angular/platform-browser-dynamic    @angular/router @angular/upgrade angular-in-memory-web-api  @angular/material angular2-highcharts rxjs core-js bootstrap ng2-bootstrap 
+npm i --registry https://registry.npm.taobao.org --save-dev tslint @angular/compiler-cli @angular/cli
+npm i --registry https://registry.npm.taobao.org
+
+
 
 npm i --save-dev typescript
 

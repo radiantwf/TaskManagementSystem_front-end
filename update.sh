12219cc81fb689009c825bf88727879a4160b8f4
radiantwf <radiantwf@gmail.com>
2017-05-05 12:32:14 +0800

    版本升级

--- update.sh
+++ update.sh
@@ -1,6 +1,20 @@
+npm i --registry https://registry.npm.taobao.org --save \
+ @angular/common @angular/compiler @angular/core @angular/animations \
+ @angular/forms @angular/http @angular/platform-browser \
+ @angular/platform-browser-dynamic  @angular/router \
+ @angular/upgrade angular-in-memory-web-api \
+ @angular/material angular2-highcharts rxjs \
+ core-js bootstrap ng2-bootstrap angular2-highcharts \
+ bootstrap core-js "zone.js"
+
+npm i --registry https://registry.npm.taobao.org --save-dev \
+ tslint @angular/compiler-cli @angular/cli @types/jasmine\
+ @types/node codelyzer jasmine-core jasmine-spec-reporter \
+ karma karma-chrome-launcher karma-cli karma-coverage-istanbul-reporter \
+ karma-jasmine karma-jasmine-html-reporter protractor ts-node \
+ typescript
+ 
 rm -rf node_modules dist tmp
-npm i --registry https://registry.npm.taobao.org --save @angular/common @angular/compiler @angular/core @angular/forms @angular/http @angular/platform-browser    @angular/platform-browser-dynamic    @angular/router @angular/upgrade angular-in-memory-web-api  @angular/material angular2-highcharts rxjs core-js bootstrap ng2-bootstrap 
-npm i --registry https://registry.npm.taobao.org --save-dev tslint @angular/compiler-cli @angular/cli
 npm i --registry https://registry.npm.taobao.org
 
 
